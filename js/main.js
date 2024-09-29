c3dca142d9a1f19f0262832c58e5683f9f7d5e4a
AbdulrahmanSE2003 <145312658+AbdulrahmanSE2003@users.noreply.github.com>
2024-09-29 15:04:18 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
 let page = document.querySelector(".landing-page");
 let arrayOfImgs = ["1.jpg","2.jpg","3.jpg","26024.jpg", "26027.jpg", "26042.jpg", "26103.jpg", "60677.jpg",];
+let preloadedImages = [];
+
+arrayOfImgs.forEach(img => {
+    const image = new Image();
+    image.src = `images/${img}`;
+    preloadedImages.push(image);
+});
+
+let currentIndex = 0;
+
 setInterval(() => {
-    let random = Math.floor(Math.random() * arrayOfImgs.length);
-    page.style.backgroundImage = `url(images/${arrayOfImgs[random]})`;
+    currentIndex = (currentIndex + 1) % preloadedImages.length; // Cycle through the images
+    page.style.backgroundImage = `url(${preloadedImages[currentIndex].src})`;
 }, 7000);
 
 let gear = document.querySelector(".setting-icon");
